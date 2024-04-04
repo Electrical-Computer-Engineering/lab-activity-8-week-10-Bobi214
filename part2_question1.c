a769691855db9d1d66de1476b8985fa9280a318c
Bobi214 <164414158+Bobi214@users.noreply.github.com>
2024-04-04 17:22:42 -0400

    Update part2_question1.c

--- part2_question1.c
+++ part2_question1.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int my_strcmp (char arr[], char btr[]) {
+	int i;
+	for ( i = 0; arr[i] != '\0' && btr[i] != '\0'; i++;) {
+			if (arr[i] !=tr[i]) {
+				return 0;
+			}
+			else{
+				return 1;
+			}
+		}
+	}
+}
+int my_strncmp (char arr[], char btr[]) {
+	for ()
+	else {
+		return 1;
+	}
+}
+int my_strcpy (char arr[], char btr[]) {
+	if (arr[j] = btr [j]]) {
+		return arr[j]
+	}
 
